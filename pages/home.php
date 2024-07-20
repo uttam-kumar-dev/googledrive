95d6f73a145bd76acfa096b6309f889b7b610072
uttam-kumar-dev <uttam.81810@gmail.com>
2024-07-20 18:59:44 +0530

    share file modal

--- pages/home.php
+++ pages/home.php
@@ -46,7 +46,7 @@ session()->set('current_url', $_SERVER['REQUEST_URI']);
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link rounded-pill px-2" href="#"> <i class="mdi mdi-account-multiple me-1"></i> Shared with me</a>
+                                <a class="nav-link rounded-pill px-2" href="<?= BASE_URL ?>pages/home.php?page=shared-files"> <i class="mdi mdi-account-multiple me-1"></i> Shared with me</a>
                             </li>
 
                             <li class="nav-item">
@@ -284,7 +284,11 @@ session()->set('current_url', $_SERVER['REQUEST_URI']);
     </div>
     <!-- end Toasts -->
 
+    <!-- contextmenu START -->
     <?php require_once '../components/contextmenu.html' ?>
+    <?php require_once '../components/share.html' ?>
+
+    <!-- contextmenu end -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
